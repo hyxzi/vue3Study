f892cabae5798bcc8d62598391375290c4175d07
tianyong_zhou <tianyong_zhou@kingdee.com>
2022-07-18 17:27:20 +0800

    前端权限控制

---
+++ .history/src/mock/index_20220707172241.js
@@ -0,0 +1,22 @@
+
+  export default {
+    url: '/geek-admin/user/login',
+    type: 'post',
+    response: config => {
+      const { username } = config.body
+      const token = tokens[username]
+
+      // mock error
+      if (user!=='dasheng') {
+        return {
+          code: 60204,
+          message: 'Account and password are incorrect.'
+        }
+      }
+
+      return {
+        code: 20000,
+        data: token
+      }
+    }
+  }
---
+++ src/pages/login.vue
@@ -0,0 +1,27 @@
+<template>
+  
+</template>
+<script setup>
+import { useStore } from "../store/minVuex"
+
+  function handleLogin () {
+    fromRef.value.validate(async valid => {
+      if (valid) {
+        loading.value = true
+        const {code, meagess} = await useStore.login(loginForm)
+        loading.value = false
+        if (code === 0) {
+          router.replace(toPath || '/')
+        } else {
+          meagess({
+            meagess: '登陆失败',
+            type: 'error'
+          })
+        }
+      } else {
+        console.log('error sumbit')
+        return false
+      }
+    })
+  }
+</script>
